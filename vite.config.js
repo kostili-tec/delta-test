130d3684b0fee4838cc6ef33c4f95e04b455d4e9
kostili-tec <weh4teyou@gmail.com>
2023-12-27 15:25:44 +0300

    chore: config packages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import autoprefixer from 'autoprefixer';
+import { resolve } from 'path';
+
+export const pathResolver = (p) => resolve(__dirname, '.', p);
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/delta-test/',
+  build: {
+    cssMinify: false,
+  },
+  css: {
+    postcss: {
+      plugins: [autoprefixer],
+    }
+  },
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': pathResolver('./src'),
+    },
+  },
+});
